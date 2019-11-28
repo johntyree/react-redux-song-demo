f1450a316a6a279927e7117aff0798c5fe3c373c
John Tyree <johntyree@gmail.com>
2019-11-27 19:30:23 -0800

    Redux is in. Barebones song list rendering

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SongList from './SongList.js';
+
+const App = () => {
+    return (
+      <div>
+        <SongList />
+      </div>
+    );
+};
+
+export default App;
